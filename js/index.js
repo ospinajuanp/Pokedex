ef926a644e47010cde6309ad208f76d436978761
Ospinajuanp <juanpo996@gmail.com>
2023-01-16 13:42:27 -0500

    Agregando url para consumo de api

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const GENERATIONS = [151,100,135,107,156,160,81];
+const API = "https://pokeapi.co/api/v2/";
+const URL_TYPE = `type/`;
+const URL_POKEMON = `pokemon/`;
+const URL_NATURE = `nature/`;
